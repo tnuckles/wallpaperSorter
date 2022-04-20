a334ed83aa287496bfc3eb9ded70132cfdf4bc7f
Trevor <trevnuckles@gmail.com>
2022-04-20 11:46:30 -0600

    added GetPdfData
    added a file for getting PDF data. call with getPdf.(function)

--- batchCreation.py
+++ batchCreation.py
@@ -8,3 +8,95 @@ from sqlitedict import SqliteDict
 from PyPDF2 import PdfFileReader, PdfFileWriter, PdfFileMerger, utils
 from io import StringIO
 import subprocess
+
+def BatchOrdersMain():
+    options = 1,2,3,4,5,6,0
+    print('\n| Main Menu > Batch Orders')
+    print('| 1. Batch Smooth Full')
+    print('| 2. Batch Smooth Samples')
+    print('| 3. Batch Woven Full')
+    print('| 4. Batch Woven Samples')
+    print('| 5. Batch the whole damn thing')
+    print('| 0. Return to Main Menu.')
+    try:
+        command = int(input('\n| Command > '))
+    except ValueError:
+        print('\n| Please enter a number, not text.')
+        return BatchOrdersMain()
+    while int(command) not in options:
+        print('\n| Not a valid choice.')
+        return BatchOrdersMain()
+    if command == 1:
+        confirm = confirmBatch('Smooth', 'Full')
+        if confirm == True:
+            batchingController('Smooth', 'Full')
+            return BatchOrdersMain()
+        elif confirm == False:
+            print('\n| Returning to Batch Orders.')
+            return BatchOrdersMain()
+    elif command == 2:
+        confirm = confirmBatch('Smooth', 'Sample')
+        if confirm == True:
+            batchingController('Smooth', 'Sample')
+            return BatchOrdersMain()
+        elif confirm == False:
+            print('\n| Returning to Batch Orders.')
+            return BatchOrdersMain()
+    elif command == 3:
+        confirm = confirmBatch('Woven', 'Full')
+        if confirm == True:
+            batchingController('Woven', 'Full')
+            return BatchOrdersMain()
+        elif confirm == False:
+            print('\n| Returning to Batch Orders.')
+            return BatchOrdersMain()
+    elif command == 4:
+        confirm = confirmBatch('Woven', 'Sample')
+        if confirm == True:
+            batchingController('Woven', 'Sample')
+            return BatchOrdersMain()
+        elif confirm == False:
+            print('\n| Returning to Batch Orders.')
+            return BatchOrdersMain()
+    elif command == 5:
+        batchingController('Woven', 'Full')
+        batchingController('Woven', 'Sample')
+        batchingController('Smooth', 'Full')
+        batchingController('Smooth', 'Sample')
+    elif command == 0:
+        print('| Returning to Main Menu.')
+        return main()
+
+def confirmBatch(material, orderSize):
+    options = 1,2
+    print('\n| Confirm: Batch', material, orderSize, 'PDFs?')
+    print('| 1. Yes')
+    print('| 2. No')
+    try:
+        command = int(input('| Command > '))
+    except ValueError:
+        print('\n| Please enter a number, not text.')
+        return confirmBatch(material, orderSize)
+    while int(command) not in options:
+        print('\n| Not a valid choice.')
+        return confirmBatch(material, orderSize)
+    if command == 1:
+        return True
+    elif command == 2:
+        return False
+
+def batchingController(material, orderSize):
+    print('\n| Starting', material, orderSize, 'Batching.')
+    materialLength = dirLookupDict['MaterialLength'][material]
+    # materialLength = int(input('\n| Please input your starting material length in feet > '))
+    # while materialLength != type(int):
+    #     materialLength = int(input('\n| Please input your starting material length in feet > '))
+    BatchDir = BatchDirBuilder(material, orderSize)
+    findOrdersForPrintv3(BatchDir, material, orderSize, (int(materialLength)))
+    removeEmptyBatchFolders(True)
+    # if orderSize == 'Full':
+    #     findOrdersForPrintv3(BatchDir, material, orderSize, (int(materialLength)))
+    #     removeEmptyBatchFolders(True)
+    # else:
+    #     findSampleOrdersForPrint(BatchDir, material, orderSize, (int(materialLength * 12)))
+    print('\n| Finished Batching', material, orderSize, 'orders.')
---
+++ getPdfData.py
@@ -0,0 +1,51 @@
+#!usr/bin/env python
+
+# Series of functions that break apart the name of a PDF and return a specific value
+
+import datetime
+
+def name(pdf):
+    return pdf.split('/')[-1].split('.pdf')[0]
+
+def friendlyName(pdf):
+    friendlyPdfName = orderNumber(pdf) + ' ' +  templateName(pdf) + ' ' +  orderItem(pdf) 
+    return friendlyPdfName
+
+def orderNumber(pdf):
+    return name(pdf).split('-')[0]
+
+def orderItem(pdf):
+    return name(pdf).split('-')[1]
+
+def dueDate(pdf):
+    return datetime.date(datetime.strptime(name(pdf).split('(')[1].split(')')[0], '%Y-%m-%d'))
+
+def shipMethod(pdf):
+    return name(pdf).split('-')[5]
+
+def material(pdf):
+    return name(pdf).split('-')[6]
+
+def size(pdf):
+    return name(pdf).split('-')[7]
+
+def repeat(pdf):
+    return int(name(pdf).split('-')[8].split('Rp ')[1])
+
+def quantity(pdf):
+    return int(name(pdf).split('-')[9].split('Qty ')[1])
+
+def oddOrEven(pdf):
+    return int(name(pdf).split('-')[9].split('Qty ')[1]) % 2
+
+def templateName(pdf):
+    return name(pdf).split('-')[10]
+
+def length(pdf):
+    return float(name(pdf).split('-')[11].split('L')[1])
+
+def width(pdf):
+    return int(name(pdf).split('-')[12].split('W')[1])
+
+def height(pdf):
+    return float(name(pdf).split('-')[13].split('H')[1])
